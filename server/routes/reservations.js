81d48cbdef97e1c595aee670ef33d27545bcd696
Amy Kim <amesk3@gmail.com>
2019-07-12 20:47:06 -0400

    first commit

--- server/routes/reservations.js
+++ server/routes/reservations.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET reservations listing. */
 router.get("/", function(req, res, next) {
   res.json([
-    { id: 1, name: "John Lenon, 7pm party of 4" },
+    { id: 1, name: "John Lennon, 7pm party of 4" },
     { id: 2, name: "Mary Berry, 1pm party of 2" }
   ]);
 });
